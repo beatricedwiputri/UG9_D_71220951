635f7a309fbf9f079ff0c79cb1202dd4194c70db
beatricedwiputri <117900796+beatricedwiputri@users.noreply.github.com>
2022-11-17 16:23:02 +0700

    Add files via upload

---
+++ 1_D_71220951.py
@@ -0,0 +1,11 @@
+print ("Test Case 1")
+phi=3.14
+r=float(input("Masukan jari-jari :"))
+luas= phi*r*r
+print("luas lingkaran adalah:", str(luas)) 
+
+print ("Test Case 2")
+phi=3.14
+r=float(input("Masukan jari-jari :"))
+luas= phi*r*r
+print("luas lingkaran adalah:", str(luas)) 
---
+++ 2_D_71220951.py
@@ -0,0 +1,13 @@
+print ("Test case 1:")
+a=float(input("Masukkan alas atap:"))
+ta=float(input("Masukkan tinggi atap:"))
+tb=float(input("Masukan tinggi tembok:"))
+luas= a*ta*tb
+print ("rumah tersebut membutuhkan", luas/5, "sak semen")
+
+print ("Test case 2:")
+a=float(input("Masukkan alas atap:"))
+ta=float(input("Masukkan tinggi atap:"))
+tb=float(input("Masukan tinggi tembok:"))
+luas= a*ta*tb
+print ("rumah tersebut membutuhkan", luas/5, "sak semen")
